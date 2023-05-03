dd0923cb1da3de78a613248c8ebb804066d2d767
Sameera Ishrat <ss1304@yahoo.com>
2023-05-03 15:39:15 -0700

    Added more components details

--- reduxmovieapp/src/components/MovieDetails/MovieDetails.js
+++ reduxmovieapp/src/components/MovieDetails/MovieDetails.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useSelector , useDispatch } from 'react-redux';
+import { fetchAsyncMovieOrShowDetail} from '../../features/movies/movieSlice';
+//import { useLocation } from 'react-router-dom';
+// import {useParams} from "react-router";
+import {useParams} from "react-router";
 const MovieDetails = () => {
+  const {imdbID} = useParams();
+
+  // let imdbID = useLocation().pathname.slice(8)
+  console.log(imdbID,"imdbID")
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.movies.selectMovieOrShow);
+
+  console.log("moviesorshows",data)
+  useEffect(() => {
+dispatch(fetchAsyncMovieOrShowDetail(imdbID))
+  },[dispatch,imdbID])
   return (
-    <div>MovieDetails</div>
+    <div style={{fontSize:"2rem", color:"#fff"}}>
+     MovieDetails
+    </div>
   )
 }
 
